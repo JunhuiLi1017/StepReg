dcdc26480553060ce13f0a797d784ca967aecf48
JunhuiLi1017 <junhuili@cau.edu.cn>
2022-12-10 22:27:27 -0500

    Initial commit

--- R/print.StepReg.R
+++ R/print.StepReg.R
@@ -27,7 +27,7 @@ print.StepReg <- function(x,...){
   
   side <- rep("both",ncol(x))
   list(colnames(x),lengths,side) %>% pmap_chr(str_pad) -> dfHeader
-  side <- rep("left",ncol(x))
+  side <- rep("right",ncol(x))
   list(x,lengths,side) %>% pmap_dfc(str_pad) -> dataFrame
   
   cat(paste0(rep("\u2017",sum(lengths)),collapse=""));cat("\n")
