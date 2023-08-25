96bbab521aab30e0664137fe672d9cec43d88b25
XiaohuanLu <xiaohuanlu.bioinfo@gmail.com>
2023-08-25 09:40:59 -0400

    add output_cox_stepwise.Rdata

--- tests/data/prepare_golden_dataset.R
+++ tests/data/prepare_golden_dataset.R
@@ -4,7 +4,7 @@ mtcars$yes <- mtcars$wt
 formula1 <- mpg ~ . + 1
 
 output_linear_stepwise <- list()
-#note1: linearStepwiseAllCombination.Rdata
+#note1: output_linear_stepwise.Rdata
 #data: mtcars$yes <- mtcars$wt
 #formula: mpg ~ . + 0
 #method: version 1.4.4
@@ -51,4 +51,32 @@ for(strategy in c("forward","backward","bidirection")){
 setwd("~/dropbox/Project/UMMS/Github/JunhuiLi1017/StepReg/tests/data")
 saveRDS(output_logit_stepwise,file="output_logit_stepwise.Rdata")
 
+# cox
+#note1: output_cox_stepwise.Rdata
+#data: lung <- survival::lung %>% na.omit()
+#formula: Surv(time, status) ~ .
+#method: version 1.4.4
 
+library(StepReg)
+library(dplyr)
+library(survival)
+
+output_cox_stepwise <- list()
+lung <- survival::lung %>% na.omit()
+formula3 = Surv(time, status) ~ .
+
+for (strategy in c("forward","backward","bidirection")){
+  for (metric in c("SL", "AIC", "AICc", "SBC", "HQ", "HQc", "IC(3/2)", "IC(1)")){
+    message(strategy,"\t",metric)
+    output_old <- NA
+    try(output_old <- stepwiseCox(formula = formula3, 
+                                  data = lung,
+                                  selection = strategy,
+                                  select = metric,
+                                  method = "efron")[[3]][,c(2,3,5)],silent = TRUE)
+    output_cox_stepwise[[strategy]][[metric]] <- output_old
+  }
+}
+
+setwd("F:/git_xiaohuan/StepReg/tests/data")
+saveRDS(output_cox_stepwise,file="output_cox_stepwise.Rdata")
