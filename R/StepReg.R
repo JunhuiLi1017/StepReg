0a275b02caf3bf71f30122419f8d9cbb6aae3d80
JunhuiLi1017 <junhuili@cau.edu.cn>
2024-01-29 13:46:40 -0500

    version updated

--- R/StepReg.R
+++ R/StepReg.R
@@ -3,7 +3,7 @@
 #' The package provides functions to perform Stepwise Regression using various algorithms, e.g., Forward Selection, Backward Elimination, and Bidirectional Elimination. Meanwhile, it also supports the Best Subset method. Multiple selection criteria (stopping criteria) are supported, including AIC, AICc, BIC, HQ, and more.
 #'
 #' \tabular{ll}{ Package: \tab StepReg\cr Type: \tab Package\cr Version:
-#' \tab 1.4.4\cr GitHub: \tab https://github.com/JunhuiLi1017/StepReg\cr Date: \tab 2023-01-23\cr }
+#' \tab 1.5.0\cr GitHub: \tab https://github.com/JunhuiLi1017/StepReg\cr Date: \tab 2023-01-23\cr }
 #'
 #' @name StepReg-package
 #' @aliases StepReg-package StepReg
@@ -11,7 +11,7 @@
 #' @author Junhui Li, Kai Hu, Xiaohuan Lu, Kun Cheng, Sushmita Nayak, Cesar Bautista Sotelo, Michael Lodato, Robert H Brown, Lihua Julie Zhu, Wenxin Liu
 #'
 #' Maintainers:\cr
-#' Junhui Li <Junhui.li11@cau.edu.cn>\cr
+#' Junhui Li <junhui.li11@umassmed.edu>\cr
 #' Kai Hu <kai.hu@umassmed.edu>
 #'
 #' @references 1. [to be added]
