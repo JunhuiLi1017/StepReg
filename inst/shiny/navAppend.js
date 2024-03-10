55e7e71f0fc134f5972712dbff199053aed619ee
JunhuiLi1017 <junhuili@cau.edu.cn>
2024-03-10 01:35:25 -0500

    navbarMenu

--- inst/shiny/navAppend.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-  $(".navbar .container-fluid .navbar-nav .dropdown .dropdown-menu").append('<li><a href="https://google.com" target="_blank">Open Sales Gsheet</a></li>');
-});
